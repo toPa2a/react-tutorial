082609ff1788744b18a34af586536b6614b311c8
toPa2a <topa2a.xtbh@gmail.com>
2020-02-05 20:31:44 +0200

    useEffect()

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,20 +1,51 @@
-import React, { useContext } from 'react';
+import React, {useState, useEffect, Component} from 'react';
 import ReactDOM from 'react-dom';
 
-const MyContext = React.createContext();
-
 const App = () => {
-    return (
-        <MyContext.Provider value="Hello World 1234">
-            <Child />
-        </MyContext.Provider>
-    );
+    const [value, setValue] = useState(0);
+    const [visible, setVisible] = useState(true);
+    
+    if (visible) {
+        return (
+            <div>
+                <button onClick={() => setValue((v) => v + 1)}>+</button>
+                <button onClick={() => setVisible(false)}>hide</button>
+                <ClassCounter value={value} />
+                <HookCounter value={value} />
+            </div>
+        );
+    } else {
+        return <button onClick={() => setVisible(true)}>show</button>
+    }
 };
 
-const Child = () => {
-    const value = useContext(MyContext);
+const HookCounter = ({value}) => {
+    useEffect(() => {
+        console.log('useEffect()');
+        
+        return () => console.log('clear');
+    }, [value]);
     
-    return <p>{value}</p>;
+    return <p>{value}</p>
 };
+
+class ClassCounter extends Component {
+    componentDidMount() {
+        console.log('class: mount');
+    }
+    
+    componentDidUpdate() {
+        console.log('class: update');
+    }
+    
+    componentWillUnmount() {
+        console.log('class: unmount');
+    }
+    
+    render() {
+        return <p>{this.props.value}</p>;
+    }
+};
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
